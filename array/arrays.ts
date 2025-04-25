03ad394746178f10219de370ef10efbb92159d26
dongun0116 <dongun16@naver.com>
2025-04-25 22:55:21 +0900

    2025.04.25-1st-arrays-ts-type.ex)

---
+++ array/arrays.ts
@@ -0,0 +1,22 @@
+const activeUsers: string[] = []
+activeUsers.push("Lim")
+
+const ageList: number[] = [45, 56, 13]
+ageList[0] = 99;
+
+const numbers: Array<number> = [];
+numbers[0] = 1;
+
+type Point = {
+    x: number,
+    y: number
+}
+
+const coords: Point[] = []
+coords.push({ x: 23, y: 8 })
+
+const board: string[][] = [
+    ["X", "O", "X"],
+    ["X", "O", "X"],
+    ["X", "O", "X"]
+]
