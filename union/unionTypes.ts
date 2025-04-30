9aa726893485fae95acc29b1ebf81fd0df9457e8
dongun0116 <dongun16@naver.com>
2025-04-30 23:27:28 +0900

    2025.04.30-1st-unionType

---
+++ union/unionTypes.ts
@@ -0,0 +1,15 @@
+let age: number | string = 23;
+age = "24"
+
+type Point = {
+    x: number;
+    y: number;
+};
+
+type Loc = {
+    lat: number;
+    long: number;
+};
+
+let coordinates: Point | Loc = { x: 1, y: 34 }
+coordinates = { lat: 321.213, long: 23.334 }
