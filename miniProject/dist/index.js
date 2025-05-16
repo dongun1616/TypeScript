b879928b1c938e0306eb100c13501beb69e38980
dongun0116 <dongun16@naver.com>
2025-05-16 22:51:29 +0900

    2025.05.16-1st-todolist first-ex

--- miniProject/dist/index.js
+++ miniProject/dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
-btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
-});
+const form = document.getElementById("todoform");
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submit");
+}
+form.addEventListener("submit", handleSubmit);
+// btn.addEventListener("click", function () {
+//     alert(input.value);
+//     input.value = "";
+// })
 // let mystery: unknown = "Hello World!"
 // const numChars = (mystery as string).length
 // console.log(numChars);
--- miniProject/src/index.ts
+++ miniProject/src/index.ts
@@ -1,12 +1,18 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.getElementById("todoform")!;
 
-btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
-})
+function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+    console.log("Submit")
+}
 
+form.addEventListener("submit", handleSubmit)
 
+// btn.addEventListener("click", function () {
+//     alert(input.value);
+//     input.value = "";
+// })
 
 // let mystery: unknown = "Hello World!"
 
