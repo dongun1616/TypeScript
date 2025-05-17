8b54ba1fcbb9021bd3929d11daea83efd49661c5
dongun0116 <dongun16@naver.com>
2025-05-18 00:30:03 +0900

    2025.05.17-2nd-todolist-localconnect-ex

--- miniProject/dist/index.js
+++ miniProject/dist/index.js
@@ -1,9 +1,22 @@
 "use strict";
-const todos = [];
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.getElementById("todoform");
 const list = document.getElementById("todolist");
+const todos = readTodos();
+todos.forEach(createTodo);
+//todo 목록을 읽어오는 함수
+function readTodos() {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+//todo 목록을 저장하는 함수
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+//폼을 제출해 todo 목록에 추가하는 함수
 function handleSubmit(e) {
     e.preventDefault();
     const newTodo = {
@@ -12,12 +25,19 @@ function handleSubmit(e) {
     };
     createTodo(newTodo);
     todos.push(newTodo);
+    saveTodos();
     input.value = "";
 }
+//todo에서 리스트와 체크박스를 생성하는 함수
 function createTodo(todo) {
     const newLi = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
     newLi.append(todo.text);
     newLi.append(checkbox);
     list.append(newLi);
--- miniProject/src/index.ts
+++ miniProject/src/index.ts
@@ -3,13 +3,27 @@ interface Todo {
     completed: boolean;
 }
 
-const todos: Todo[] = []
-
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.getElementById("todoform")!;
 const list = document.getElementById("todolist")!;
 
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+//todo 목록을 읽어오는 함수
+function readTodos(): Todo[] {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null) return [];
+    return JSON.parse(todosJSON)
+}
+
+//todo 목록을 저장하는 함수
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+//폼을 제출해 todo 목록에 추가하는 함수
 function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
     const newTodo: Todo = {
@@ -19,13 +33,22 @@ function handleSubmit(e: SubmitEvent) {
     createTodo(newTodo)
     todos.push(newTodo);
 
+    saveTodos();
     input.value = "";
 }
 
+//todo에서 리스트와 체크박스를 생성하는 함수
 function createTodo(todo: Todo) {
     const newLi = document.createElement("li")
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox"
+    checkbox.checked = todo.completed;
+
+    checkbox.addEventListener("change", function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    })
+
     newLi.append(todo.text)
     newLi.append(checkbox)
     list.append(newLi)
