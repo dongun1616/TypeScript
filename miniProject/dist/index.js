ecf017592cb8699bfc4a3c1e3435126074657cde
dongun0116 <dongun16@naver.com>
2025-05-17 23:50:30 +0900

    2025.05.17-1st-todolist-interface

--- miniProject/dist/index.js
+++ miniProject/dist/index.js
@@ -1,10 +1,26 @@
 "use strict";
+const todos = [];
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.getElementById("todoform");
+const list = document.getElementById("todolist");
 function handleSubmit(e) {
     e.preventDefault();
-    console.log("Submit");
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = "";
+}
+function createTodo(todo) {
+    const newLi = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLi.append(todo.text);
+    newLi.append(checkbox);
+    list.append(newLi);
 }
 form.addEventListener("submit", handleSubmit);
 // btn.addEventListener("click", function () {
--- miniProject/src/index.ts
+++ miniProject/src/index.ts
@@ -1,10 +1,34 @@
+interface Todo {
+    text: string;
+    completed: boolean;
+}
+
+const todos: Todo[] = []
+
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.getElementById("todoform")!;
+const list = document.getElementById("todolist")!;
 
 function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
-    console.log("Submit")
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false,
+    }
+    createTodo(newTodo)
+    todos.push(newTodo);
+
+    input.value = "";
+}
+
+function createTodo(todo: Todo) {
+    const newLi = document.createElement("li")
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox"
+    newLi.append(todo.text)
+    newLi.append(checkbox)
+    list.append(newLi)
 }
 
 form.addEventListener("submit", handleSubmit)
