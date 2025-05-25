dbdfa0ad5f602676d493c4b8e5eaa541757fc544
dongun0116 <dongun16@naver.com>
2025-05-25 23:52:19 +0900

    2025.05.25-1st-getter,setter,protected-ex)

--- class/dist/index.js
+++ class/dist/index.js
@@ -1,13 +1,51 @@
 "use strict";
+// class Player {
+//     readonly first: string;
+//     readonly last: string;
+//     private score: number = 0;
+//     // #score: number = 0;
+//     constructor(first: string, last: string) {
+//         this.first = first;
+//         this.last = last
+//     }
+//     private secretMethod(): void {
+//         console.log("secret method!")
+//     }
+// }
 class Player {
-    // #score: number = 0;
-    constructor(first, last) {
-        this.score = 0;
+    constructor(first, last, _score) {
         this.first = first;
         this.last = last;
+        this._score = _score;
     }
     secretMethod() {
         console.log("secret method!");
     }
+    //풀네임을 가져오는 getter설정
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    //score점수를 가져오는 getter설정
+    get score() {
+        return this._score;
+    }
+    //score점수를 수정하는 setter설정
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score canot be negative!");
+        }
+        this._score = newScore;
+    }
 }
-const elton = new Player("Elton", "Steele");
+class SuperPlayer extends Player {
+    constructor() {
+        super(...arguments);
+        this.idAdmin = true;
+    }
+    maxScore() {
+        this._score = 9999;
+    }
+}
+const elton = new Player("Elton", "Steele", 100);
+elton.fullName;
+elton.score = 99;
--- class/src/index.ts
+++ class/src/index.ts
@@ -1,15 +1,53 @@
+// class Player {
+//     readonly first: string;
+//     readonly last: string;
+//     private score: number = 0;
+//     // #score: number = 0;
+//     constructor(first: string, last: string) {
+//         this.first = first;
+//         this.last = last
+//     }
+
+//     private secretMethod(): void {
+//         console.log("secret method!")
+//     }
+// }
+
 class Player {
-    readonly first: string;
-    readonly last: string;
-    private score: number = 0;
-    // #score: number = 0;
-    constructor(first: string, last: string) {
-        this.first = first;
-        this.last = last
-    }
+    constructor(
+        public first: string,
+        public last: string,
+        protected _score: number
+    ) { }
 
     private secretMethod(): void {
         console.log("secret method!")
     }
+    //풀네임을 가져오는 getter설정
+    get fullName(): string {
+        return `${this.first} ${this.last}`;
+    }
+
+    //score점수를 가져오는 getter설정
+    get score(): number {
+        return this._score
+    }
+    //score점수를 수정하는 setter설정
+    set score(newScore: number) {
+        if (newScore < 0) {
+            throw new Error("Score canot be negative!")
+        }
+        this._score = newScore;
+    }
 }
-const elton = new Player("Elton", "Steele")
+
+class SuperPlayer extends Player {
+    public idAdmin: boolean = true;
+    maxScore() {
+        this._score = 9999
+    }
+}
+
+const elton = new Player("Elton", "Steele", 100)
+elton.fullName;
+elton.score = 99;
